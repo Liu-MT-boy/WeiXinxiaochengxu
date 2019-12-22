a08d33e136075610aa809266ca972fada7990079
liuwei <liuwei@itcast.cn>
2019-12-22 20:42:51 +0800

    4-完成商品列表的tabs栏，还在封装的myAxios中添加下拉刷新结束和关闭导航小菊花功能

--- pages/goods_list/index.js
+++ pages/goods_list/index.js
@@ -1,29 +1,51 @@
 // pages/goods_list/index.js
-// 引入封装myAxios
-import { myAxios } from '../../utils/myAxios.js'
+// 引入全局实例
+const app = getApp();
+// 在需要使用到  async await 的 js 中，手动引入 runtime.js， regeneratorRuntime 名字不能改
+import regeneratorRuntime from '../../lib/runtime/runtime'
+
 Page({
   
   /**
    * 页面的初始数据
    */
   data: {
-    productlist:[]
+    productlist:[],
+    // tabs 数据
+    activeIndex:0,
+    // tabs栏
+    tabs : [
+      {
+        id:1,
+        name:'综合'
+      },
+      {
+        id:2,
+        name:'销量'
+      },
+      {
+        id:3,
+        name:'价格'
+      }
+    ]
+  },
+  // 点击切换 tabs 的索引
+  changeIndex (event) {
+    // console.log(event);
+    const { index } = event.currentTarget.dataset
+    this.setData({
+      activeIndex : index
+    })
+    
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad (oprions) {
+  async onLoad (oprions) {
     console.log(oprions);
-    wx.request({
-      url: 'https://api.zbztb.cn/api/public/v1/goods/search',
-      success:res=> {
-        console.log(res);
-        this.setData({
-          productlist : res.data.message.goods
-        })
-        
-      }
-    })
+    const productlist = await app.myAxios({url:'goods/search'})
+
+    this.setData({productlist})
   }
 })
