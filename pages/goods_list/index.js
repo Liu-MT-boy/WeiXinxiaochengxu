d2bbd16f5ef8a6706644e143cd75c5901f3e3ac1
liuwei <liuwei@itcast.cn>
2019-12-23 09:36:58 +0800

    5-通过阿里妈妈引入svg图标、完成列表右边样式、页面成功加载并完成下拉重新加载事件

--- pages/goods_list/index.js
+++ pages/goods_list/index.js
@@ -10,7 +10,14 @@ Page({
    * 页面的初始数据
    */
   data: {
-    productlist:[],
+     // 商品分页
+     pagenum:1,
+     // 请求一次的数量
+     pagesize:10,
+     // 总条数
+     total:0,
+     // 商品列表
+     goods:[],
     // tabs 数据
     activeIndex:0,
     // tabs栏
@@ -39,13 +46,36 @@ Page({
     
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  async onLoad (oprions) {
-    console.log(oprions);
-    const productlist = await app.myAxios({url:'goods/search'})
+  // 页面加载生命函数
+  async onLoad (options) {
 
-    this.setData({productlist})
+    // 根据参数发送请求
+    let {pagenum,pagesize} = this.data
+    const res = await app.myAxios({
+      url:'goods/search',
+      data : {
+        ...options,
+        pagenum,
+        pagesize
+      }
+    })
+    console.log(res);
+    // 更新页面的列表，并把总数保存起来用于做分页
+    this.setData({
+      goods:res.goods,
+      total:res.total
+    })
+  },
+
+  // 下拉事件
+  onPullDownRefresh () {
+    // 把商品数组清空，把页码重新变成1
+    this.setData({
+      goods:[],
+      pagenum:1
+    })
+    // 重新调用一下onLoad,重新加载
+    this.onLoad(this.options)
+
   }
 })
