487324ae7f9e70d23a46f60946fa42b0f6d03b28
liuwei <liuwei@itcast.cn>
2020-01-05 17:50:41 +0800

    支付页面改造完成，封装的myAxios中重新添加新功能

--- pages/pay/index.js
+++ pages/pay/index.js
@@ -1,66 +1,43 @@
-// pages/pay/index.js
+// pages/cart/index.js
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    // address: wx.getStorageSync('address') || {}
+    // 在 data 中写的获取也只会触发一次
+    address: {},
+    cartList: [],
+    totalPrice: 0
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+  // 页面开启时运行
+  onShow(){
+    // 每次显示后获取最新本地存储数据，并更新到 data 中
+    this.setData({
+      address : wx.getStorageSync('address') || {},
+      cartList : wx.getStorageSync('cartList') || []
+    })
+    this.computedCartData()
   },
+  // 封装一个计算总价格的函数
+  computedCartData() {
+    const { cartList } = this.data;
+    // 总价格
+    let totalPrice = 0;
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
+    // 计算总价格
+    cartList.forEach(v => {
+      // 如果是选中的商品
+      if (v.goods_selected){
+        // 总金额
+        totalPrice += v.goods_price * v.goods_count;
+      }
+    })
 
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+    // 更新数据 页面和本地存储
+    this.setData({
+      totalPrice,
+    })
   }
 })
