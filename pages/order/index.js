93d22e7622b7908daa244ea2a81d40d007972f61
liuwei <liuwei@itcast.cn>
2020-02-01 20:34:09 +0800

    订单页面的头部完成

--- pages/order/index.js
+++ pages/order/index.js
@@ -6,61 +6,40 @@ Page({
    */
   data: {
 
+    // tabs 数据
+    activeIndex:0,
+    // tabs栏
+    tabs : [
+      {
+        id:1,
+        name:'全部'
+      },
+      {
+        id:2,
+        name:'待付款'
+      },
+      {
+        id:3,
+        name:'代发货'
+      },
+      {
+        id:4,
+        name:'退款/退货'
+      }
+    ]
+  },
+  changeIndex (event) {
+    console.log(event);
+    const { index } = event.target.dataset
+    this.setData({
+      activeIndex:index
+    })
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
 
   }
 })
